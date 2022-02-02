4c5bcdff01057d9c2eb24b2495727bb47f1b8610
Damien Hubleur <hubleur.damien333@protonmail.ch>
2022-02-02 19:42:07 +0100

    complete hpp file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import { commands, ExtensionContext, Position, ViewColumn, WebviewPanel, window } from 'vscode';
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { type } from 'os';
 
 function getGuiHtml() {
     let guiHTML =  `
@@ -14,28 +15,53 @@ function getGuiHtml() {
 			body{
 				background-color:#17202A;
 				color: #FFFFFF;
+				font-family: Arial, Helvetica, sans-serif;
 			}
 			.button {
-                background-color: Transparent;
-                background-repeat:no-repeat;
-                border: none;
-                cursor:pointer;
-                overflow: absolute;
-                outline:none;
-                padding: 12px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 12px;
-                margin: 4px 2px;
-                transition-duration: 0.4s;
+				background-color: Transparent;
+				background-repeat:no-repeat;
+				border: none;
+				cursor:pointer;
+				overflow: absolute;
+				outline:none;
+				padding: 12px;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 12px;
+				margin: 4px 2px;
+				transition-duration: 0.4s;
 				width:200px;
 				margin-top: 30px;
 				margin-left: calc(50% - 100px);
 				color:white;
 				border: 2px solid #008CBA;
-            }
+			}
 			.button:hover{
+				background-color: #008CBA;
+				color: black;
+			}
+			.button2 {
+				background-color: Transparent;
+				background-repeat:no-repeat;
+				border: none;
+				cursor:pointer;
+				overflow: absolute;
+				outline:none;
+				padding: 12px;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 12px;
+				margin: 4px 2px;
+				transition-duration: 0.4s;
+				width:150px;
+				margin-top: 30px;
+				margin-left: calc(50% - 75px);
+				color:white;
+				border: 2px solid #008CBA;
+			}
+			.button2:hover{
 				background-color: #008CBA;
 				color: black;
 			}
@@ -54,7 +80,44 @@ function getGuiHtml() {
 				width: 300px;
 				margin-left: calc(50% - 150px);
 				margin-top: 5px;
-				height: 12px
+				height: 12px;
+				margin-bottom: 10px;
+			}
+			.type-input{
+				background-color: Transparent;
+				background-repeat:no-repeat;
+				border: 1px solid #008CBA;
+				color: #ffffff;
+				overflow: absolute;
+				outline:none;
+				padding: 12px;
+				text-align: center;
+				text-decoration: none;
+				display: inline;
+				font-size: 22px;
+				width: 100px;
+				margin-top: 5px;
+				height: 12px;
+				margin-bottom: 10px;
+				margin-left: 30%;
+			}
+			.name-input{
+				background-color: Transparent;
+				background-repeat:no-repeat;
+				border: 1px solid #008CBA;
+				color: #ffffff;
+				overflow: absolute;
+				outline:none;
+				padding: 12px;
+				text-align: center;
+				text-decoration: none;
+				display: inline;
+				font-size: 22px;
+				width: 300px;
+				margin-top: 5px;
+				height: 12px;
+				margin-bottom: 10px;
+				margin-left: 50px;
 			}
 			.label{
 				color: #ffffff;
@@ -64,20 +127,91 @@ function getGuiHtml() {
 				margin-left: calc(50% - 150px);
 				margin-top: 20px;
 			}
+			.label2{
+				color: #ffffff;
+				text-align: center;
+				font-family: "Arial";
+				font-size: 17px;
+				margin-left: 30%;
+				margin-top: 20px;
+			}
+			.label3{
+				color: #ffffff;
+				text-align: center;
+				font-family: "Arial";
+				font-size: 17px;
+				margin-left: 100px;
+				margin-top: 20px;
+			}
 		</style>
 	</head>
 	<body>
 		<form id="generation-form">
 			<label class="label">Class name:</label> <br />
 			<input type="text" class="input" id="className" placeholder="ClassName"/><br />
-			<button class="button" onclick="generate()">Generate</button>
+			<div id="fields">
+			</div>
+			<div class="button2" onclick="addField()">Add a field</div>
+			<div class="button" onclick="generate()">Generate</div>
 		</form>
 		<script>
+			let field = 0;
 			const vscode = acquireVsCodeApi();
 			function generate() {
+				let fieldsList = [];
+				if(document.getElementById("className").value == ""){
+					vscode.postMessage({
+						type: 'error'
+					});
+					return ;
+				}
+				for(let i = 0; i < field; i++){
+					if(document.getElementById("field_type_" + i).value == "" && document.getElementById("field_name_" + i).value == "")
+					{
+						continue;
+					}
+					if(document.getElementById("field_type_" + i).value == "" || document.getElementById("field_name_" + i).value == "")
+					{
+						vscode.postMessage({
+							type: 'error2'
+						});
+						return ;
+					}
+					fieldsList[i] = {field_type: document.getElementById("field_type_" + i).value, field_name: document.getElementById("field_name_" + i).value};
+				}
 				vscode.postMessage({
-					className: document.getElementById('className').value
+					type: "generate",
+					className: document.getElementById('className').value,
+					fields: fieldsList
 				});
+			}
+			function addField() {
+				let div = document.createElement("div");
+				div.className = "field";
+				document.getElementById('fields').appendChild(div);
+				let label = document.createElement("label");
+				label.className = "label2";
+				label.innerHTML = "Field type:";
+				div.appendChild(label);
+				label = document.createElement("label");
+				label.className = "label3";
+				label.innerHTML = "Field name:";
+				div.appendChild(label);
+				let br = document.createElement("br");
+				div.appendChild(br);
+				let type = document.createElement("input");
+				type.className = "type-input";
+				type.type = "text";
+				type.id = "field_type_" + field;
+				type.placeholder = "Field type";
+				div.appendChild(type);
+				let name = document.createElement("input");
+				name.className = "name-input";
+				name.type = "text";
+				name.id = "field_name_" + field;
+				name.placeholder = "Field name";
+				div.appendChild(name);
+				field++;
 			}
 		</script>
 	</body>
@@ -96,7 +230,7 @@ export function activate(context: ExtensionContext) {
 		onePanel = window.createWebviewPanel(
 			'cppgenerator',
 			'Class Generator',
-			{ viewColumn: ViewColumn.Two, preserveFocus: true },
+			{ viewColumn: ViewColumn.One, preserveFocus: true },
 			{
 				enableScripts: true,
 			}
@@ -104,63 +238,98 @@ export function activate(context: ExtensionContext) {
 		onePanel.webview.html = getGuiHtml();
 		onePanel.webview.onDidReceiveMessage(
 			async (message: any) => {
-				console.log("Start generating class " + message.className);
-
-				if(vscode.workspace.workspaceFolders !== undefined)
+				if(message.type === "error")
 				{
-					{
-						let text = "#ifndef " + message.className.toUpperCase() + "_HPP\n# define " + message.className.toUpperCase() + "_HPP\n\n";
-						text += "# include <iostream>\n";
-						text += "# include <string>\n\n";
-						text += "class " + message.className + "\n{\n";
-						text += "\tpublic:\n";
-						text += "\t\t" + message.className + "();\n";
-						text += "\t\t" + message.className + "(const " + message.className + " &copy);\n";
-						text += "\t\t~" + message.className + "();\n";
-						text += "\t\t" + message.className + " &operator=(const " + message.className + " &assign);\n";
-						text += "\n";
-						text += "\tprivate:\n\t\t\n";
-						text += "};\n\n";
-						text += "#endif";
-
-						const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, message.className + ".hpp"));
-						vscode.workspace.openTextDocument(newFile).then(document => {
-							const edit = new vscode.WorkspaceEdit();
-							edit.insert(newFile, new vscode.Position(0, 0), text);
-							return vscode.workspace.applyEdit(edit).then(success => {
-								if (success) {
-									document.save();
-									vscode.window.showInformationMessage('HPP created !');
-								} else {
-									vscode.window.showInformationMessage('Error during HPP creation !');
-								}
-							});
-						});
-					}
-					{
-						let text = "#include \"" + message.className + ".hpp\"\n\n";
-
-						const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, message.className + ".cpp"));
-						vscode.workspace.openTextDocument(newFile).then(document => {
-							const edit = new vscode.WorkspaceEdit();
-							edit.insert(newFile, new vscode.Position(0, 0), text);
-							return vscode.workspace.applyEdit(edit).then(success => {
-								if (success) {
-									document.save();
-									vscode.window.showInformationMessage('CPP created !');
-								} else {
-									vscode.window.showInformationMessage('Error during CPP creation !');
-								}
-							});
-						});
-					}
+					vscode.window.showErrorMessage('Please specify a class name');
 				}
-				else
+				if(message.type === "error2")
 				{
-					console.error("Impossible to localise user");
+					vscode.window.showErrorMessage('One of the field is incomplete');
 				}
+				if(message.type === "generate")
+				{
+					console.log("Start generating class " + message.className);
 
-				return;
+					if(vscode.workspace.workspaceFolders !== undefined)
+					{
+						{
+							let text = "#ifndef " + message.className.toUpperCase() + "_HPP\n# define " + message.className.toUpperCase() + "_HPP\n\n";
+							text += "# include <iostream>\n";
+							text += "# include <string>\n\n";
+							text += "class " + message.className + "\n{\n";
+							text += "\tpublic:\n";
+							text += "\t\t" + message.className + "();\n";
+							text += "\t\t" + message.className + "(const " + message.className + " &copy);\n";
+							text += "\t\t" + message.className + "(";
+							for(let i = 0; i < message.fields.length; i++)
+							{
+								if(i !== 0)
+								{
+									text += ", ";
+								}
+								text += message.fields[i].field_type + " " + message.fields[i].field_name;
+							}
+							text += ");\n";
+							text += "\t\t~" + message.className + "();\n";
+							text += "\t\t" + message.className + " & operator=(const " + message.className + " &assign);\n";
+							text += "\t\t\n";
+							for(let i = 0; i < message.fields.length; i++)
+							{
+								text += "\t\t" + message.fields[i].field_type + " get" + message.fields[i].field_name[0].toUpperCase() + message.fields[i].field_name.slice(1) + "();\n";
+								text += "\t\tvoid set" + message.fields[i].field_name[0].toUpperCase() + message.fields[i].field_name.slice(1) + "(" + message.fields[i].field_type + " " + message.fields[i].field_name + ");\n";
+							}
+							text += "\t\t\n";
+							text += "\tprivate:\n";
+							for(let i = 0; i < message.fields.length; i++)
+							{
+								text += "\t\t" + message.fields[i].field_type + " _" + message.fields[i].field_name + ";\n";
+							}
+							text += "\t\t\n";
+							text += "};\n\n";
+							text += "#endif";
+
+							const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, message.className + ".hpp"));
+							vscode.workspace.openTextDocument(newFile).then(document => {
+								const edit = new vscode.WorkspaceEdit();
+								edit.insert(newFile, new vscode.Position(0, 0), text);
+								return vscode.workspace.applyEdit(edit).then(success => {
+									if (success) {
+										document.save();
+										vscode.window.showInformationMessage('HPP created !');
+									} else {
+										vscode.window.showErrorMessage('Error during HPP creation !');
+									}
+								});
+							});
+						}
+						{
+							let text = "#include \"" + message.className + ".hpp\"\n\n";
+							text += message.className + "::" + message.className + "()\n{\n";
+							text += "\t\n";
+							text += "}\n\n";
+
+							const newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, message.className + ".cpp"));
+							vscode.workspace.openTextDocument(newFile).then(document => {
+								const edit = new vscode.WorkspaceEdit();
+								edit.insert(newFile, new vscode.Position(0, 0), text);
+								return vscode.workspace.applyEdit(edit).then(success => {
+									if (success) {
+										document.save();
+										vscode.window.showInformationMessage('CPP created !');
+									} else {
+										vscode.window.showErrorMessage('Error during CPP creation !');
+									}
+								});
+							});
+						}
+					}
+					else
+					{
+						console.error("Impossible to localise user");
+					}
+
+					return;
+				}
 			},
 			undefined,
 			context.subscriptions
